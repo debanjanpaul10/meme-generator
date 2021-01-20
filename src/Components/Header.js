2f537423ee4e5036493cf214925f30b494d9e063
Debanjan Paul <debanjanpaul10@gmail.com>
2021-01-20 12:30:55 +0530

    Second Push

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,11 @@
 import React from "react"
+import headerImage from "../Images/Trollface.png"
 
 export default function Header() {
     return (
         <header>
             <img 
-                src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+                src={headerImage} 
                 alt="Problem?" 
             />
             <p>Meme Generator</p>
